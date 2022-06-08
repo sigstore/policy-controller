177cb560ac0dbd7ad412dabe1e1ee4877992b8a8
cpanato <ctadeu@gmail.com>
2022-06-08 16:14:09 +0200

    fix build job
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- test/ci.mk
+++ test/ci.mk
@@ -2,15 +2,10 @@
 # signing ci
 ############
 
-.PHONY: sign-policy-controller
-sign-policy-controller:
-	cosign sign --key .github/workflows/cosign-test.key -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/policy-controller:$(GIT_HASH)
+.PHONY: sign-policy-images
+sign-policy-images:
+	cosign sign -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/policy-controller:$(GIT_HASH)
+	cosign sign -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/policy-webhook:$(GIT_HASH)
 
-.PHONY: sign-keyless-policy-controller
-sign-keyless-policy-controller:
-	cosign sign -a sha=$(GIT_HASH) -a run_id=${GITHUB_RUN_ID} -a run_attempt=${GITHUB_RUN_ATTEMPT} ${KO_PREFIX}/policy-controller:$(GIT_HASH)
-	cosign sign -a sha=$(GIT_HASH) -a run_id=${GITHUB_RUN_ID} -a run_attempt=${GITHUB_RUN_ATTEMPT} ${KO_PREFIX}/policy-controller:$(GIT_VERSION)
-
-
-.PHONY: sign-keyless-container
-sign-keyless-container: ko sign-keyless-policy-controller
+.PHONY: build-sign-containers
+build-sign-containers: ko sign-policy-images
