c1e4c209788b2c62c0368e5d5b29c2ef1b656b1b
cpanato <ctadeu@gmail.com>
2022-06-09 13:10:00 +0200

    update release job
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- release/release.mk
+++ release/release.mk
@@ -4,29 +4,20 @@
 # used when releasing together with GCP CloudBuild
 .PHONY: release
 release:
-	LDFLAGS="$(LDFLAGS)" goreleaser release --parallelism 1 --timeout 120m
+	LDFLAGS="$(LDFLAGS)" goreleaser release --timeout 120m
 
 ######################
 # sign section
 ######################
 
-.PHONY: sign-release-images
-sign-release-images: ko
+.PHONY: build-sign-release-images
+build-sign-release-images: ko
 	GIT_HASH=$(GIT_HASH) GIT_VERSION=$(GIT_VERSION) \
 	./release/ko-sign-release-images.sh
-
-# used when need to validate the goreleaser
-.PHONY: snapshot
-snapshot:
-	LDFLAGS="$(LDFLAGS)" goreleaser release --skip-sign --skip-publish --snapshot --rm-dist --timeout 60m
 
 ####################
 # copy image to GHCR
 ####################
-
-.PHONY: copy-cosign-signed-release-to-ghcr
-copy-cosign-signed-release-to-ghcr:
-	cosign copy $(KO_PREFIX)/cosign:$(GIT_VERSION) $(GHCR_PREFIX)/cosign:$(GIT_VERSION)
 
 .PHONY: copy-policy-controller-signed-release-to-ghcr
 copy-policy-controller-signed-release-to-ghcr:
@@ -36,9 +27,5 @@ copy-policy-controller-signed-release-to-ghcr:
 copy-policy-webhook-signed-release-to-ghcr:
 	cosign copy $(KO_PREFIX)/policy-webhook:$(GIT_VERSION) $(GHCR_PREFIX)/policy-webhook:$(GIT_VERSION)
 
-.PHONY: copy-sget-signed-release-to-ghcr
-copy-sget-signed-release-to-ghcr:
-	cosign copy $(KO_PREFIX)/sget:$(GIT_VERSION) $(GHCR_PREFIX)/sget:$(GIT_VERSION)
-
 .PHONY: copy-signed-release-to-ghcr
-copy-signed-release-to-ghcr: copy-cosign-signed-release-to-ghcr copy-policy-controller-signed-release-to-ghcr copy-sget-signed-release-to-ghcr copy-policy-webhook-signed-release-to-ghcr
+copy-signed-release-to-ghcr: copy-policy-controller-signed-release-to-ghcr copy-policy-webhook-signed-release-to-ghcr
