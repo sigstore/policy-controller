80c98b2e2c7347ff9afe9cc509e60cc6cf45086a
Hector Fernandez <hector@chainguard.dev>
2023-02-01 16:42:44 +0100

    fix policy conversion from v1beta1 to v1alpha1
    Signed-off-by: Hector Fernandez <hector@chainguard.dev>

--- pkg/apis/policy/v1alpha1/clusterimagepolicy_conversion_test.go
+++ pkg/apis/policy/v1alpha1/clusterimagepolicy_conversion_test.go
@@ -291,6 +291,23 @@ func TestConversionRoundTripV1beta1(t *testing.T) {
 				},
 			},
 		},
+	}, {name: "key, keyless, and rfc3161timestamp, regexp",
+		in: &v1beta1.ClusterImagePolicy{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test-cip",
+			},
+			Spec: v1beta1.ClusterImagePolicySpec{
+				Images: []v1beta1.ImagePattern{{Glob: "*"}},
+				Authorities: []v1beta1.Authority{
+					{Keyless: &v1beta1.KeylessRef{
+						Identities:        []v1beta1.Identity{{SubjectRegExp: "subjectregexp", IssuerRegExp: "issuerregexp"}},
+						CACert:            &v1beta1.KeyRef{KMS: "kms", Data: "data", SecretRef: &v1.SecretReference{Name: "secret"}},
+						InsecureIgnoreSCT: ptr.Bool(true),
+					},
+					},
+				},
+			},
+		},
 	}}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
