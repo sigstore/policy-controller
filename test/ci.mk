32c9ffc3d846e0557aeb76c2f7277849b1e5892e
Hector Fernandez <hector@chainguard.dev>
2023-04-11 13:15:39 +0200

    update tests
    Signed-off-by: Hector Fernandez <hector@chainguard.dev>

--- test/ci.mk
+++ test/ci.mk
@@ -5,7 +5,6 @@
 .PHONY: sign-policy-images
 sign-policy-images:
 	cosign sign -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/policy-controller:$(GIT_HASH)
-	cosign sign -a GIT_HASH=$(GIT_HASH) ${KO_PREFIX}/policy-webhook:$(GIT_HASH)
 
 .PHONY: build-sign-containers
 build-sign-containers: ko sign-policy-images
